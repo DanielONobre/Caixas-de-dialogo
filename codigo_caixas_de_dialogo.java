2e59a978f7f607eab3ac02c6c24f1529aefe62e4
Carlos Daniel Farias Nobre <106852463+DanielONobre@users.noreply.github.com>
2022-08-06 17:22:18 -0300

    Add files via upload

---
+++ codigo_caixas_de_dialogo.java
@@ -0,0 +1,38 @@
+package caixadialogo;
+
+import javax.swing.JOptionPane;
+
+
+public class codigo_caixas_de_dialogo {
+	
+	public static void main(String[] args) {
+
+		String carros = JOptionPane.showInputDialog("Informe a quantidade de carros");
+		String pessoas = JOptionPane.showInputDialog("Informe a quantidade de pessoas");
+
+		double carroNumero = Double.parseDouble(carros);
+		double pessoaNumero = Double.parseDouble(pessoas);
+
+		int divisao = (int) (carroNumero / pessoaNumero);
+
+		double resto = carroNumero % pessoaNumero;
+
+		int resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resultado da divisão?");
+
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null, "Divisão para pessoas deu " + divisao);
+		} else {
+			System.out.println("Não quis ver o resultado");
+		}
+
+		resposta = JOptionPane.showConfirmDialog(null, "Deseja ver o resto da divisão?");
+
+		if (resposta == 0) {
+			JOptionPane.showMessageDialog(null, "O resto da divisão é " + resto);
+		} else {
+			System.out.println("Não quis ver o resultado");
+		}
+
+	}
+
+}
